a124841683c1a95f5a2d931d02ee133f366722f3
AndreFrainer <dedesf2004@gmail.com>
2025-06-28 12:34:46 -0300

    CUCUCUCUCU

--- L2.ml
+++ L2.ml
@@ -69,7 +69,7 @@ let is_unit (e: expr) : bool =
   | Unit -> true
   | _ -> false
 
-let bop_step (op: bop, e1: expr, e2: expr) : expr =
+let bop_step (op, e1, e2 : bop*expr*expr) : expr =
   match op, e1, e2 with
   | Sum, Num n1, Num n2 -> Num (n1 + n2)
   | Sub, Num n1, Num n2 -> Num (n1 - n2)
@@ -83,14 +83,19 @@ let bop_step (op: bop, e1: expr, e2: expr) : expr =
   | Or, Bool b1, Bool b2 -> Bool (b1 || b2)
   | _ -> failwith "Invalid binary operation"
 
-let empty_pos_mem (mem: int*bool array) : int =
+let empty_pos_mem (mem : (expr * bool) array) : int =
   let i = ref 0 in
   while !i < Array.length mem && snd mem.(!i) do
     i := !i + 1
   done;
+  !i
 
+let int_expr (e: expr) : int =
+  match e with
+  | Num n -> n
+  | _ -> failwith "Nao é inteiro"
 
-let rec subst_var (e: expr, var: string, v: expr) : expr =
+let rec subst_var (e,var,v : expr*string*expr) : expr =
   match e with
   | Id id when id=var -> v
   | Id _ | Num _ | Bool _ | Unit | Read -> e
@@ -106,74 +111,92 @@ let rec subst_var (e: expr, var: string, v: expr) : expr =
   
   | Let(var1, tp, e1, e2) when var=var1 -> Let(var1, tp, subst_var(e1,var,v), e2)
   | Let(var1, tp, e1, e2) -> Let(var1, tp, subst_var(e1,var,v), subst_var(e2,var,v)) 
+                            
 
-let rec small_step ( e: expr, mem: int*bool array, entrada: int list, saida: int list ) : expr * int array * int list * int list =
+let rec small_step ((e, mem, entrada, saida) : expr * (expr * bool) array * int list * int list) : expr * (expr * bool) array * int list * int list =
   match e with
-| Num _ | Bool _ | Unit -> (e, mem, entrada, saida)
-| Binop (op, e1, e2) when is_val e1 && is_val e2 ->
-    (bop_step(op, e1, e2), mem, entrada, saida)
-| Binop (op, e1, e2) when is_val e1 ->
-    (Binop(op, e1, small_step(e2, mem, entrada, saida)), mem, entrada, saida)
-| Binop (op, e1, e2) -> 
-    (Binop(op, small_step(e1, mem, entrada, saida), e2), mem, entrada, saida)
-| If (e1, e2, e3) when is_val e1 ->
-    (match e1 with
-     | Bool true -> (e2, mem, entrada, saida)
-     | Bool false -> (e3, mem, entrada, saida)
-     | _ -> failwith "Condition must be a boolean")
-| If (e1, e2, e3) -> 
-    (If(small_step(e1, mem, entrada, saida), e2, e3), mem, entrada, saida)
-| Seq (e1, e2) when is_unit e1 ->
-    (e2, mem, entrada, saida)
-| Seq (e1, e2) -> 
-    (Seq(small_step(e1, mem, entrada, saida),e2), mem, entrada, saida)
-| Wh (e1, e2) -> 
-    (If(e1, Seq(e2, Wh(e1,e2)), (Unit, mem, entrada, saida)))
-| Asg (e1, e2) when is_val e1 && is_val e2 ->
-    (Unit, mem.(e1)<-(e2, true), entrada, saida)
-| Asg (e1, e2) when is_val e1 ->
-    (Asg(e1, small_step(e2, mem, entrada, saida)), mem, entrada, saida)
-| Asg (e1, e2) -> 
-    (Asg(small_step(e1, mem, entrada, saida), e2), mem, entrada, saida)
-| Let (var, tp, e1, e2) when is_val e1 ->  
-    (subst_var(e2, var, e1), mem, entrada, saida)
-| Let (var, tp, e1, e2) ->
-    (Let(var, tp, small_step(e1, mem, entrada, saida), e2), mem, entrada, saida)
-| New (e1) when is_val e1 ->
-    let pos = empty_pos_mem mem in
-    let new_mem = Array.copy mem in
-    new_mem.(pos) <- (e1, true);
-    (pos, new_mem, entrada, saida)
-| New (e1) -> 
-    (New(small_step(e1, mem, entrada, saida)), mem, entrada, saida)
-| Deref (e1) when is_val e1 ->
-    (fst(mem.(e1)), mem, entrada, saida)
-| Deref (e1) ->
-    (Deref(small_step(e1, mem, entrada, saida)), mem, entrada, saida)
-| Print (e1) when is_val e1 ->
-    (Unit, mem, entrada, e1::saida)
-| Print (e1) ->
-    (Print(small_step(e1, mem, entrada, saida)), mem, entrada, saida)
-| Read ->
-  match entrada with
-  | [] -> failwith "Entrada vazia!"
-  | h::t -> (Num h, mem, t, saida) 
+  | Num _ | Bool _ | Unit | Id _ -> (e, mem, entrada, saida)
+  | Binop (op, e1, e2) when is_val e1 && is_val e2 ->
+      (bop_step (op, e1, e2), mem, entrada, saida)
+  | Binop (op, e1, e2) when is_val e1 ->
+      let (e2', mem', entrada', saida') = small_step (e2, mem, entrada, saida) in
+      (Binop (op, e1, e2'), mem', entrada', saida')
+  | Binop (op, e1, e2) ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (Binop (op, e1', e2), mem', entrada', saida')
+  | If (e1, e2, e3) when is_val e1 ->
+      (match e1 with
+       | Bool true -> (e2, mem, entrada, saida)
+       | Bool false -> (e3, mem, entrada, saida)
+       | _ -> failwith "Condition must be a boolean")
+  | If (e1, e2, e3) ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (If (e1', e2, e3), mem', entrada', saida')
+  | Seq (e1, e2) when is_unit e1 ->
+      (e2, mem, entrada, saida)
+  | Seq (e1, e2) ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (Seq (e1', e2), mem', entrada', saida')
+  | Wh (e1, e2) ->
+      (If (e1, Seq (e2, Wh (e1, e2)), Unit), mem, entrada, saida)
+  | Asg (Num addr, v) when is_val v ->
+      let new_mem = Array.copy mem in
+      new_mem.(addr) <- (v, true);
+      (Unit, new_mem, entrada, saida)
+  | Asg (e1, e2) when is_val e1 ->
+      let (e2', mem', entrada', saida') = small_step (e2, mem, entrada, saida) in
+      (Asg (e1, e2'), mem', entrada', saida')
+  | Asg (e1, e2) ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (Asg (e1', e2), mem', entrada', saida')
+  | Let (var, tp, e1, e2) when is_val e1 ->
+      (subst_var (e2, var, e1), mem, entrada, saida)
+  | Let (var, tp, e1, e2) ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (Let (var, tp, e1', e2), mem', entrada', saida')
+  | New e1 when is_val e1 ->
+      let pos = empty_pos_mem mem in
+      let new_mem = Array.copy mem in
+      new_mem.(pos) <- (e1, true);
+      (Num pos, new_mem, entrada, saida)
+  | New e1 ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (New e1', mem', entrada', saida')
+  | Deref (Num addr) ->
+      (fst mem.(addr), mem, entrada, saida)
+  | Deref e1 ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (Deref e1', mem', entrada', saida')
+  | Print e1 when is_val e1 ->
+      let valor = int_expr e1 in
+      (Unit, mem, entrada, valor :: saida)
+  | Print e1 ->
+      let (e1', mem', entrada', saida') = small_step (e1, mem, entrada, saida) in
+      (Print e1', mem', entrada', saida')
+  | Read ->
+      (match entrada with
+       | [] -> failwith "Entrada vazia!"
+       | h :: t -> (Num h, mem, t, saida))
 
-let main () =
-  let mem = Array.make 100 (Unit, false) in
-  let entrada = [5] in
+let main (entrada: int list) =
+  (* 1. Initialize the state *)
+  let mem = Array.make 100 (Unit, false) in 
   let saida = [] in
-  let rec loop e =
-    match small_step(e, mem, entrada, saida) with
-    | (Unit, _, _, saida) -> List.rev saida
-    | (e', mem', entrada', saida') -> loop e'
+
+  (* 2. The loop must pass the state along in each recursive call *)
+  let rec loop (e, mem, entrada, saida) =
+    match small_step (e, mem, entrada, saida) with
+    | (Unit, _, _, final_saida) -> List.rev final_saida (* Base case: program finished *)
+    | (e', mem', entrada', saida') -> loop (e', mem', entrada', saida') (* Recursive step with new state *)
   in
-  loop fat
 
+  (* 3. Call the loop with the initial state and capture the result *)
+  let result_list = loop (fat, mem, entrada, saida) in
 
-
-
-
-
+  (* 4. Iterate through the result list and print each element *)
+  print_endline "Output:";
+  List.iter (fun my_int -> (* The value is already an int *)
+      print_int my_int;      (* Print it directly *)
+      print_newline ()
+    ) result_list
   
-    
